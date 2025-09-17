556eda79396ad9bbdb245b981302e0ae03befdd1
ITM16141 <i.atsuki611@gmail.com>
2025-09-17 12:49:22 +0900

    変数名を修正

--- gemini-api-v2/sample/sample.ts
+++ gemini-api-v2/sample/sample.ts
@@ -3,8 +3,8 @@ import {Chat} from "../src/geminiChat";
 const chat = new Chat();
 
 (async () => {
-    const reply1 = await chat.ask("こんにちは");
-    console.log("Gemini:", reply1);
+    const reply = await chat.ask("こんにちは");
+    console.log("Gemini:", reply);
 
     chat.resetSession(); // セッションをリセット
 })();
